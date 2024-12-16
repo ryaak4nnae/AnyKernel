5aa243d87905d80b059353ed3db775904626d06a
dragonzords <xyzuan1608@gmail.com>
2024-12-16 19:03:09 +0700

    Update anykernel.sh

--- anykernel.sh
+++ anykernel.sh
@@ -4,14 +4,14 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=NoVA by Abdul7852
+kernel.string=Hatsune-Miku by hnkksk
 do.devicecheck=1
 do.modules=0
 do.systemless=1
 do.cleanup=1
 do.cleanuponabort=1
-device.name1=begonia
-device.name2=begoniain
+device.name1=mido
+device.name2=mido
 supported.versions=
 supported.patchlevels=
 '; } # end properties
@@ -50,7 +50,7 @@ if isTimRom; then
 
     ui_print " » Custom ROM recognition: Tim's Signature ";
     ui_print " » Initiating vibration fix deployment "
-    ui_print " » Executing NoVA flash....";
+    ui_print " » Executing Hatsune-Miku Flasher....";
 
     split_boot;
     patch_cmdline initcall_blacklist initcall_blacklist=
@@ -72,7 +72,7 @@ elif isDerp; then
     rm -rf $ramdisk/overlay.d;
 
     ui_print " » Custom ROM recognition: DerpFest's detected ";
-    ui_print " » Executing NoVA flash....";
+    ui_print " » Executing Hatsune-Miku Flasher....";
 
     dump_boot;
     write_boot;
@@ -91,7 +91,7 @@ else
     rm -rf $ramdisk/overlay;
     rm -rf $ramdisk/overlay.d;
 
-    ui_print " » Executing NoVA flash....";
+    ui_print " » Executing Hatsune-Miku Flasher...";
 
     dump_boot;
     write_boot;
