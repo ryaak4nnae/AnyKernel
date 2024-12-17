fc6ba02ffa8e6b9ff1bf9bdef350f7b63b7cebb2
dragonzords <xyzuan1608@gmail.com>
2024-12-17 14:40:31 +0700

    Update anykernel.sh

--- anykernel.sh
+++ anykernel.sh
@@ -4,14 +4,14 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=Hatsune-Miku by hnkksk
+kernel.string=Barong-kernel by hnkksk
 do.devicecheck=1
 do.modules=0
 do.systemless=1
 do.cleanup=1
 do.cleanuponabort=1
-device.name1=vayu
-device.name2=vayu
+device.name1=begonia
+device.name2=begoniain
 supported.versions=
 supported.patchlevels=
 '; } # end properties
@@ -50,7 +50,7 @@ if isTimRom; then
 
     ui_print " » Custom ROM recognition: Tim's Signature ";
     ui_print " » Initiating vibration fix deployment "
-    ui_print " » Executing Ram (ラム) Flasher....";
+    ui_print " » Executing Barong Flasher Let's Beast Your G90T....";
 
     split_boot;
     patch_cmdline initcall_blacklist initcall_blacklist=
@@ -72,7 +72,7 @@ elif isDerp; then
     rm -rf $ramdisk/overlay.d;
 
     ui_print " » Custom ROM recognition: DerpFest's detected ";
-    ui_print " » Executing Ram (ラム) Flasher....";
+    ui_print " » Executing Barong Flasher Let's Beast Your G90T...";
 
     dump_boot;
     write_boot;
@@ -91,7 +91,7 @@ else
     rm -rf $ramdisk/overlay;
     rm -rf $ramdisk/overlay.d;
 
-    ui_print " » Executing Ram (ラム) Flasher...";
+    ui_print " » Executing Barong Flasher let's beast your G90T...";
 
     dump_boot;
     write_boot;
